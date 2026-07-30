20027f14d7cf62725404b4248ecf899fa782db38
refullstudio <108417024+refullstudio@users.noreply.github.com>
2026-07-30 16:43:03 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const app = document.getElementById("app");
+let path = window.location.hash.replace("#","") || "/";
 
-app.innerHTML = `
-    <h1>RUSHINGS</h1>
-    <p>Rushings is working.</p>
-`;
+fetch("https://api.rushings.xyz/page" + path)
+.then(r => r.text())
+.then(html => {
+    document.body.innerHTML = html;
+})
+.catch(() => {
+    document.body.innerHTML = "Failed loading page";
+});
